b75e5e403b56dc73e577bdec225f9bd5e1cca140
saba-0013 <sabasuki.0013@gmail.com>
2022-05-11 01:52:27 +0900

    edit totalize file

--- totalize/totalize.js
+++ totalize/totalize.js
@@ -1,44 +1,43 @@
-function RandomizeElTop(el) {
+function randomizeElTop (el) {
   // el: target element caught by messageId
-
-  // get random numbers. If more lower or higher, change each number.
+  // get random numbers. If want more lower or higher, change each number.
   const randomTop = getRandomInt(10, 75)
 
   // update top position.
   el.style.top = randomTop + '%'
 }
 
-// from Mozilla official code.
-// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
-function getRandomInt(min, max) {
+function getRandomInt (min, max) {
+  // from Mozilla official code
+  // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
   return Math.floor(Math.random() * (max - min) + min)
 }
 
 document.addEventListener('onEventReceived', function (obj) {
   // obj will contain information about the event
-  const messageId = obj['detail']['messageId']
+  const messageId = obj.detail.messageId
   if (!messageId) {
     return null
   } else {
-    const message = obj['detail']['body']
+    const message = obj.detail.body
 
-    // add childnodes(div > div > span)
-    const topdiv_ = document.createElement('div')
-    topdiv_.setAttribute('id', 'top_')
+    // add suffix _nico for escape id duplicate
+    const nicoId = `${messageId}_nico`
 
+    // create div and span in div(id=log)
     const div_ = document.createElement('div')
-    div_.setAttribute('id', messageId)
+    div_.setAttribute('id', nicoId)
 
     const span_ = document.createElement('span')
-    span_.setAttribute('class', 'nico_message')
+    span_.setAttribute('class', 'message')
     span_.innerText = message
+
+    const topdiv_ = document.getElementById('log')
 
     div_.appendChild(span_)
     topdiv_.appendChild(div_)
-    document.getElementById('custom_html').appendChild(topdiv_)
 
     // randomize el height
-    const el = document.getElementById(obj['detail']['messageId'])
-    el ? RandomizeElTop(el) : ''
+    randomizeElTop(document.getElementById(nicoId))
   }
 })
